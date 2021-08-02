e824b8464cc49df8c545c3892e0564a256a6935a
angelastra <aflt210493@gmail.com>
2021-08-02 08:30:24 -0500

    Video 5:Componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
+//DECORADOR, LE DA FUNCIONALIDAD A LA CLASE DE ABAJO
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', //NOMBRE DEL COMPONENTE, ES EL QUE SE PONE EN EL HTML PRINCIPAL
+  templateUrl: './app.component.html', //LA VISTA DEL COMPONENTE
+  styleUrls: ['./app.component.css'] //ESTILOS PARA LA VISTA DE ARRIBA
 })
 export class AppComponent {
   title = 'AprendiendoAngular';
